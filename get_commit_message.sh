9d0d5ae7eacf0d6b7a856a9b6e154a7dfdd9f0ea
renjith <renjith.rajasekharan@atmecs.com>
2020-12-14 17:42:34 +0530

    updated ci

--- get_commit_message.sh
+++ get_commit_message.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$(node -pe 'JSON.parse(process.argv[1]).commit.message.match(/(?<=commit\s\w+\s).*/g)[0]' "$(curl -s \
+node -pe 'JSON.parse(process.argv[1]).commit.message.match(/(?<=commit\s\w+\s).*/g)[0]' "$(curl -s \
 -H "Travis-API-Version: 3" \
 -H "User-Agent: API Explorer" \
 -H "Authorization: token MTDTD5ilUbB5_1jGnM3yFA" \
-https://api.travis-ci.com/build/${TRAVIS_BUILD_ID})")
+https://api.travis-ci.com/build/${TRAVIS_BUILD_ID})"
